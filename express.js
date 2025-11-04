1ecbecf6d141c72f83d5dd1aaf465d924bf10d34
joshiAditi2005 <aditisjoshi2005@gmail.com>
2025-11-04 16:39:21 +0530

    API methods implemented

--- express.js
+++ express.js
@@ -1,14 +1,19 @@
 import express from 'express'
 
 const app = express()
-
+app.use(express.json())
 const PORT = 5000
+
+
+
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })
+
 app.post('/create',(req,res)=>{
-    res.send('This is the post')
+    const {name,email,password}=req.body
+    res.json({name,email,password})
 })
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
