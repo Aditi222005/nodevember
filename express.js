f7fc921b907dfdbcf893598536cb7be439809752
joshiAditi2005 <aditisjoshi2005@gmail.com>
2025-11-04 15:59:50 +0530

    API methods implemented

--- express.js
+++ express.js
@@ -7,7 +7,9 @@ const PORT = 5000
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })
-
+app.post('/create',(req,res)=>{
+    res.send('This is the post')
+})
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
